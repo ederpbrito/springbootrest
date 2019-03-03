46c458748454eab447c9ab2c4279b2b6b9e423cf
Eder Brito <eder.brito@resourceit.com>
2019-03-03 19:11:27 -0300

    commit initial

---
+++ src/main/java/com/packtpub/HelloController.java
@@ -0,0 +1,13 @@
+package com.packtpub;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@RequestMapping("/greet")
+	public String hello(){
+		return "Hello from jetty server";
+	}
+}
