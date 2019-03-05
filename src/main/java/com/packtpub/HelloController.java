69bf7a6762f1948ffea10f2d2cd80436148e88e4
Eder Brito <ederpsilva@htomail.com>
2019-03-04 23:07:56 -0300

    include controller initial

--- src/main/java/com/packtpub/HelloController.java
+++ src/main/java/com/packtpub/HelloController.java
@@ -6,6 +6,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+	@RequestMapping("/")
+	public String initial(){
+		return "Page initial jetty server";
+	}
+	
 	@RequestMapping("/greet")
 	public String hello(){
 		return "Hello from jetty server";
